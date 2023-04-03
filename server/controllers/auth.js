0206c89b4226df1745d0d0cdd8a2739fed9723ea
LinaKv <kvitka.p@yandex.ru>
2023-04-03 17:34:39 +0400

    login added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,6 +6,7 @@ import User from "../models/User.js";
 export const register = async (req, res) => {
   try {
     const { firstName, lastName, email, password, picturePath, friends, location, occupation } = req.body;
+    // то, что мы отправляем
 
     // random salt
     const salt = await bcrypt.genSalt();
@@ -25,6 +26,33 @@ export const register = async (req, res) => {
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// LOGIN
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ msg: "UserDoes not exist" });
+    }
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      delete user.password;
+
+      res.status(200).json({
+        token,
+        user,
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid");
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
