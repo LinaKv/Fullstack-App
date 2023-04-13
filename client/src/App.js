7b81b27bae028517e6e2310f390e6519dd73f4ad
LinaKv <kvitka.p@yandex.ru>
2023-04-13 16:55:16 +0400

    home page and userwidjet were added

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { themeSettings } from "theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className='app'>
@@ -24,11 +25,11 @@ function App() {
             />
             <Route
               path='/home'
-              element={<HomePage />}
+              element={isAuth ? <HomePage /> : <Navigate to='/' />}
             />
             <Route
               path='/profile/:userId'
-              element={<ProfilePage />}
+              element={isAuth ? <ProfilePage /> : <Navigate to='/' />}
             />
           </Routes>
         </ThemeProvider>
