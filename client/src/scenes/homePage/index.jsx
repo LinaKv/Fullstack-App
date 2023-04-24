592a7faaa72630848db22cce839ba9231fce4fb1
LinaKv <kvitka.p@yandex.ru>
2023-04-24 16:53:20 +0400

    Added profile page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,8 @@ import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 function HomePage() {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
@@ -37,9 +39,9 @@ function HomePage() {
 
         {isNonMobileScreens && (
           <Box flexBasis='26%'>
-            {/* <AdvertWidget /> */}
+            <AdvertWidget />
             <Box m='2rem 0' />
-            {/* <FriendListWidget userId={_id} /> */}
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
